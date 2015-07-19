604a91b6ae74e735ee19f7bf93c3ff79a4ec5691
Gabor Csatlos <neyma6@gmail.com>
2015-07-19 22:20:13 +0200

    no message

--- src/com/gabor/csatlos/controller/ImageController.java
+++ src/com/gabor/csatlos/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.gabor.csatlos.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,24 +11,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gabor.csatlos.domain.ErrorStatus;
+import com.gabor.csatlos.service.ImageService;
 import com.gabor.csatlos.utils.ResponseBuilder;
 
 @Controller
 @RequestMapping("/image")
 public class ImageController {
 
+	@Autowired
+	private ImageService imageService;
+	
 	@ResponseBody
 	@RequestMapping(value = "/upload", method = RequestMethod.GET)
-	public Map<String, Object> upload() {
+	public Map<String, Object> upload(@RequestParam String id, @RequestParam String imageData) {
 		
-		return null;
+		return imageService.upload(id, imageData);
 	}
 	
 	@ResponseBody
 	@RequestMapping(value = "/get", method = RequestMethod.GET)
 	public Map<String, Object> get(@RequestParam String id) {
 		
-		return null;
+		return imageService.get(id);
 	}
 	
 	@ExceptionHandler(Exception.class)
