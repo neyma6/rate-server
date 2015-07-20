d0900bc083021a152c3dbddee336c1b70ad05d27
Gabor_Csatlos <Gabor_Csatlos@epam.com>
2015-07-20 14:23:33 +0200

    Add blob controller test endpoint

--- src/com/gabor/csatlos/controller/BlobController.java
+++ src/com/gabor/csatlos/controller/BlobController.java
@@ -6,12 +6,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gabor.csatlos.domain.ErrorStatus;
+import com.gabor.csatlos.domain.ResponseParam;
 import com.gabor.csatlos.service.BlobService;
 import com.gabor.csatlos.utils.ResponseBuilder;
 
@@ -29,7 +31,7 @@ public class BlobController {
 	}
 	
 	@ResponseBody
-	@RequestMapping(value = "/upload")
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public Map<String, Object> upload(HttpServletRequest request) {
 		return blobService.upload(request);
 	}
@@ -38,5 +40,16 @@ public class BlobController {
 	@ResponseBody
 	public Map<String, Object> handleBadRequest() {
 		return ResponseBuilder.sendError(ErrorStatus.INVALID_PARAMETER);
+	}
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(ModelMap model) {
+		
+		Map<String, Object> response = blobService.generateUploadUrl("/blob/upload");
+		String url = (String)response.get(ResponseParam.URL.getName());
+		
+		model.addAttribute("imageUrl", url);
+		
+		return "testblob";
 	}
 }
---
+++ war/pages/testblob.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+	<form action="${imageUrl}" method="post" enctype="multipart/form-data">
+        <input type="file" name="defaultImage">
+        <input type="text" name="userid">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
